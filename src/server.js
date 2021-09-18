395ad3a4eb87904ed3ca968b9a3358c76fcea2a1
Léo Grambert <leogrambert@protonmail.com>
2021-09-18 10:53:23 +0200

    feat: create expressjs server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,37 @@
-require('dotenv').config();
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-  res.end('API MARKETPLACE');
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) return val;
+  if (port >= 0) return port;
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+const address = server.address();
+const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') throw error;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use.`);
+      break;
+    default:
+      throw error;
+  }
+  process.exit(1);
+};
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  console.log('Listening on ' + bind);
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(port);
