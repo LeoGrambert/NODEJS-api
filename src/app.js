38f49ffad827505696daf60fcb1f72cac2b0a70d
Léo Grambert <leogrambert@protonmail.com>
2021-10-10 22:29:41 +0200

    refacto: create controllers and routes folders

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('./helpers/logger');
-const Product = require('./models/product');
+const productRoutes = require('./routes/product');
 
 const app = express();
 
@@ -29,39 +29,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.post('/api/products', (req, res) => {
-  delete req.body._id;
-  const product = new Product({
-    ...req.body,
-  });
-  product
-    .save()
-    .then(() => res.status(201).json({ message: 'Product created' }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get('/api/products/:id', (req, res) => {
-  Product.findOne({ _id: req.params.id })
-    .then((product) => res.status(200).json(product))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-app.put('/api/products/:id', (req, res) => {
-  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Product updated' }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.delete('/api/products/:id', (req, res) => {
-  Product.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Product deleted' }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.use('/api/products', (req, res) => {
-  Product.find()
-    .then((products) => res.status(200).json(products))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use('/api/products', productRoutes);
 
 module.exports = app;
